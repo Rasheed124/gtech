f1fe189ccdb3a8b600f408990651d6ef15e41948
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-08-11 19:06:17 +0100

    zappy template page rendered

--- public/script.js
+++ public/script.js
@@ -1,9 +1,24 @@
-const hamburger = document.getElementById("hamburger");
-const navToggleElements = document.getElementsByClassName("toggle");
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const menuContainer = document.getElementById("menuContainer");
 
-hamburger.onclick = function toggleMenu() {
-  for (let i = 0; i < navToggleElements.length; i++) {
-    const element = navToggleElements.item(i);
-    element.classList.toggle("hidden");
-  }
-};
+console.log(openMenu, closeMenu, menuContainer);
+
+openMenu.addEventListener("click", () => {
+    menuContainer.classList.toggle("translate-y-0")
+
+    if(!menuContainer.classList.contains("flex")){
+        menuContainer.classList.add("flex")
+    }
+})
+
+closeMenu.addEventListener("click", () => {
+    menuContainer.classList.toggle("translate-y-0")
+
+    if(!menuContainer.classList.contains("flex")){
+        menuContainer.classList.add("flex")
+    }
+})
+
+
+
