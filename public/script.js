122d8dcd37be5b7e6a08d9ecb333ec69460dca71
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-08-14 10:11:35 +0100

    gtech template page rendered

--- public/script.js
+++ public/script.js
@@ -1,24 +1,27 @@
-const openMenu = document.getElementById("openMenu");
-const closeMenu = document.getElementById("closeMenu");
-const menuContainer = document.getElementById("menuContainer");
+const openMenu = document.getElementById('openMenu');
+const closeMenu = document.getElementById('closeMenu');
+const menuContainer = document.getElementById('menuContainer');
 
 console.log(openMenu, closeMenu, menuContainer);
 
-openMenu.addEventListener("click", () => {
-    menuContainer.classList.toggle("translate-y-0")
+openMenu.addEventListener('click', () => {
+  menuContainer.classList.toggle('translate-y-0');
 
-    if(!menuContainer.classList.contains("flex")){
-        menuContainer.classList.add("flex")
-    }
-})
+  if (!menuContainer.classList.contains('flex')) {
+    menuContainer.classList.add('flex');
+  }
 
-closeMenu.addEventListener("click", () => {
-    menuContainer.classList.toggle("translate-y-0")
+  document.body.classList.add('overflow-hidden');
+});
 
-    if(!menuContainer.classList.contains("flex")){
-        menuContainer.classList.add("flex")
-    }
-})
+closeMenu.addEventListener('click', () => {
+  menuContainer.classList.toggle('translate-y-0');
 
+  if (!menuContainer.classList.contains('flex')) {
+    menuContainer.classList.add('flex');
+  }
 
+  document.body.classList.remove("overflow-hidden");
+});
 
+AOS.init();
