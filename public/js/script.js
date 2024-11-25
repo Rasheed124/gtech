ef0b3fbe4fd46975ab1d2b2bb663a9410562851e
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-11-25 22:36:49 +0100

    added notification

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
+// Select buttons and menu
+const openMenuButton = document.getElementById("openMenuButton");
+const closeMenuButton = document.getElementById("closeMenuButton");
+const sideMobileMenu = document.getElementById("sideMobileMenu");
+
+const menuContainer = document.querySelector("#sideMobileMenu .menuContainer");
+
 const notificationBtn = document.getElementById("notification-btn");
 const notificationDropdown = document.getElementById("notification-dropdown");
 
@@ -8,7 +15,29 @@ notificationBtn.addEventListener("click", () => {
 
 // Close dropdown when clicking outside
 document.addEventListener("click", (e) => {
-  if (!notificationBtn.contains(e.target) && !notificationDropdown.contains(e.target)) {
+  if (
+    !notificationBtn.contains(e.target) &&
+    !notificationDropdown.contains(e.target)
+  ) {
     notificationDropdown.classList.add("hidden");
+  }
+});
+
+// Function to show the menu
+openMenuButton.addEventListener("click", () => {
+  sideMobileMenu.classList.remove("-translate-x-[1000%]");
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !notificationBtn.contains(e.target) &&
+    !notificationDropdown.contains(e.target)
+  ) {
+    notificationDropdown.classList.add("hidden");
+  }
+
+  // Side Mobile Menu
+  if (sideMobileMenu.contains(e.target) && !menuContainer.contains(e.target)) {
+    sideMobileMenu.classList.add("-translate-x-[1000%]");
   }
 });
