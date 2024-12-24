3e493f3ca83c3bf3f85f06dcff5641497bdca90f
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-12-24 17:02:09 +0100

    changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -144,11 +144,8 @@ module.exports = {
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
-        NexaLight: ["NexaLight", "sans-serif"],
-        NexaThin: ["NexaThin", "sans-serif"],
-        NexaBold: ["NexaBold", "sans-serif"],
-        NexaBook: ["NexaBook", "sans-serif"],
-        NexaXBold: ["NexaXBold", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+   
         
       },
       fontSize: {
